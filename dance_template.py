d97584e456d152cae9d256798f43db8253654d70
Eben Quenneville <student.equenneville@gmail.com>
2023-07-20 21:38:41 -0400

    finishing choreography. adds error checking and documentation

--- dance_template.py
+++ dance_template.py
@@ -3,8 +3,8 @@ import time
 from pymavlink import mavutil
 
 ## Globals
-turn_time = 5
 power = 100
+
 
 def arm_rov(mav_connection):
     """
@@ -34,6 +34,24 @@ def run_motors_timed(mav_connection, seconds: int, motor_settings: list) -> None
     :param motor_settings: The motor settings, a list of 6 values -100 to 100
     :return: None
     """
+    # Error checking
+    for i, power in enumerate(motor_settings):
+        if not isinstance(power, int):
+            try:
+                motor_settings[i] = int(power)
+            except:
+                motor_settings[i] = 0
+        if power > 100:
+            motor_settings[i] = 100
+        elif power < -100:
+            motor_settings[i] = -100
+
+    if not isinstance(seconds, int):
+        try:
+            seconds = int(round(seconds))
+        except:
+            seconds = 0
+
     step = 0
     while step < seconds:
         for i in range(len(motor_settings)):
@@ -45,34 +63,81 @@ def run_motors_timed(mav_connection, seconds: int, motor_settings: list) -> None
 
 
 def forward(mav_connection, percent: int, t: int):
+    """Move forward, using 4 thrusters.
+
+    Args:
+        mav_connection: the connection to the ROV
+        percent (int): the percentage of maximum power to set the thrusters to
+        t (int): the amount of time to apply the forces for
+    """
     run_motors_timed(
         mav_connection, t, [-1 * percent, -1 * percent, 1 * percent, 1 * percent, 0, 0]
     )
 
 
 def backward(mav_connection, percent: int, t: int):
+    """Move backward, using 4 thrusters.
+
+    Args:
+        mav_connection: the connection to the ROV
+        percent (int): the percentage of maximum power to set the thrusters to
+        t (int): the amount of time to apply the forces for
+    """
     run_motors_timed(
         mav_connection, t, [1 * percent, 1 * percent, -1 * percent, -1 * percent, 0, 0]
     )
 
 
 def up(mav_connection, percent: int, t: int):
+    """Move up.
+
+    Args:
+        mav_connection: the connection to the ROV
+        percent (int): the percentage of maximum power to set the thrusters to
+        t (int): the amount of time to apply the forces for
+    """
     run_motors_timed(mav_connection, t, [0, 0, 0, 0, 1 * percent, 1 * percent])
 
 
 def down(mav_connection, percent: int, t: int):
+    """Move down.
+
+    Args:
+        mav_connection: the connection to the ROV
+        percent (int): the percentage of maximum power to set the thrusters to
+        t (int): the amount of time to apply the forces for
+    """
     run_motors_timed(mav_connection, t, [0, 0, 0, 0, -1 * percent, -1 * percent])
 
 
 def right(mav_connection, percent: int, t: int):
-    run_motors_timed(mav_connection, t, [1 * percent, -1 * percent, 1 * percent, -1 * percent, 0, 0])
+    """Move to the right.
+
+    Args:
+        mav_connection: the connection to the ROV
+        percent (int): the percentage of maximum power to set the thrusters to
+        t (int): the amount of time to apply the forces for
+    """
+    run_motors_timed(
+        mav_connection, t, [1 * percent, -1 * percent, 1 * percent, -1 * percent, 0, 0]
+    )
 
 
 def left(mav_connection, percent: int, t: int):
-    run_motors_timed(mav_connection, t, [-1 * percent, 1 * percent, -1 * percent, 1 * percent, 0, 0])
+    """Move to the left
+
+    Args:
+        mav_connection: the connection to the ROV
+        percent (int): the percentage of maximum power to set the thrusters to
+        t (int): the amount of time to apply the forces for
+    """
+    run_motors_timed(
+        mav_connection, t, [-1 * percent, 1 * percent, -1 * percent, 1 * percent, 0, 0]
+    )
 
 
 def donut(mav_connection, percent: int):
+    """Completes a donut. Currently untested."""
     donut_time = 15
     run_motors_timed(
         mav_connection, donut_time, [1 * percent, 0, 0.5 * percent, 0.5 * percent, 0, 0]
@@ -80,6 +145,7 @@ def donut(mav_connection, percent: int):
 
 
 def circle(mav_connection):
+    """Completes a circle. Currently untested."""
     circle_time = 60
     percent = 100
     run_motors_timed(
@@ -89,45 +155,106 @@ def circle(mav_connection):
     )
 
 
-def stop(mav_connection):
-    stop_time = 5
+def stop(mav_connection, time: int = 5):
+    """Stops the AUV by setting all thrusters to 0.
+
+    Args:
+        mav_connection: the connection to the ROV
+        time (int, optional): amount of time to stop for. Defaults to 5.
+    """
+    run_motors_timed(mav_connection, seconds=time, motor_settings=[0, 0, 0, 0, 0, 0])
+
+
+def cw_turn(mav_connection, percent: int, time: int):
+    """Turn clockwise by using one thruster. The angle of the turn depends on power and time.
+
+    Args:
+        mav_connection: the connection to the ROV
+        percent (int): the percentage of full power to use
+        time (int): the time to turn for
+    """
     run_motors_timed(
-        mav_connection, seconds=stop_time, motor_settings=[0, 0, 0, 0, 0, 0]
+        mav_connection, seconds=time, motor_settings=[1 * percent, 0, 0, 0, 0, 0]
     )
 
+
+def ccw_turn(mav_connection, percent: int, time: int):
+    """Turn counterclockwise by using one thruster. The angle of the turn depends on power and time.
+
+    Args:
+        mav_connection: the connection to the ROV
+        percent (int): the percentage of full power to use
+        time (int): the time to turn for
+    """
+    run_motors_timed(
+        mav_connection, seconds=time, motor_settings=[0, 1 * percent, 0, 0, 0, 0]
+    )
+
+
 def trace_m(mav_connection, time):
-    # time = time to trace each line, i.e. the time from bottom of | to the top
-    pass
+    """Traces a 'M' in the QGroundcontrol/ardupilot simulation environment. Starts at the bottom left leg facing up, ends facing the same way at the bottom right leg.
+    Currently uses hardcoded turn durations.
+
+    Args:
+        mav_connection: the connection to the ROV
+        time: the time for each section; controls size of the 'M'
+    """
+    forward(mav_connection, power, time)  # left leg
+    cw_turn(mav_connection, 55, 9)
+    forward(mav_connection, power, time * 0.5)  # down right
+    ccw_turn(mav_connection, 50, 6)
+    forward(mav_connection, power, time * 0.5)  # up right
+    cw_turn(mav_connection, 55, 9)
+    stop(mav_connection, 2)
+    forward(mav_connection, power, time)  # right leg
+    cw_turn(mav_connection, 55, 13)  # end facing the same way we started
+
 
 def trace_i(mav_connection, time):
-    # starts at the bottom
+    """Traces a 'I' in the QGroundcontrol/ardupilot simulation environment. Starts and ends by moving a bit to the side.
+
+    Args:
+        mav_connection: the connection to the ROV
+        time: the time for each section; controls size of the 'I'
+    """
     right(mav_connection, power, time / 2)
+    left(mav_connection, power, 1)  # cancel momentum before going up
     forward(mav_connection, power, time)
     backward(mav_connection, power, time)
     right(mav_connection, power, time / 2)
-    pass
+
 
 def trace_t(mav_connection, time):
-    # starts at the bottom of the outer rectangle
+    """Traces a 'T' in the QGroundcontrol/ardupilot simulation environment. Starts at the bottom left of the bounding rectangle.
+
+    Args:
+        mav_connection: the connection to the ROV
+        time: the time for each section; controls size of the 'T'
+    """
     right(mav_connection, power, time / 2)
     forward(mav_connection, power, time)
-    left(mav_connection, power, time / 2)
-    right(mav_connection, power, time)
-    '''
+    backward(mav_connection, power, 1)
+    left(mav_connection, power, time * 0.75)
+    right(mav_connection, power, time * 1.2)
+    """
     for this choreography, we chose to end T in the top right
     to make the "MIT" more clear. Generally, it would be better
     practice to start and end in consistent places.
     If we revists this function, we will add a:
     backward(mav_connection, power, time)
-    '''
-    pass
+    """
 
 
-def trace_mit(mav_connection, time: int):
-    trace_m(mav_connection, time)
-    right(mav_connection, power, 3) # m ends at the bottom right
-    trace_i(mav_connection, time)
-    trace_t(mav_connection, time)
+def trace_mit(mav_connection):
+    """Traces out "MIT" in the QGroundControl/ardupilot simulation environment
+
+    Args:
+        mav_connection: the connection to the ROV
+    """
+    trace_m(mav_connection, 20)
+    right(mav_connection, power, 3)  # m ends at the bottom right
+    trace_i(mav_connection, 15)
+    trace_t(mav_connection, 15)
 
 
 def main():
@@ -146,17 +273,10 @@ def main():
     Call sequence of calls to run_timed_motors to execute choreography
     Motors power ranges from -100 to 100
     """
-    # dance moves here:
-    # forward(mav_connection, power, 10)
-    # backward(mav_connection, power, 11)
-    # right(mav_connection, power, 10)
-    # left(mav_connection, power, 11)
-    # donut(mav_connection, 100)
-    # circle(mav_connection)
-    trace_t(mav_connection, 20)
+    cw_turn(mav_connection, 50, 7)  # rotate to north
+    trace_mit(mav_connection)
 
-    # stop
-    stop(mav_connection)
+    stop(mav_connection)  # stop
 
     ####
     # Disarm ROV and exit
